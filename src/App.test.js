453d56aeab9cecf1829f184a51dd46c1b09c6066
Joel Ross <joelross@uw.edu>
2016-11-21 08:21:08 -0800

    Add partially completed version

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import sinon from 'sinon';
+
 import {DogList, DogCard} from './PetApp';
 import AdoptPage from './AdoptDog';
 import DOG_DATA from './dog-data'; //load the dog data to use
@@ -23,17 +25,28 @@ describe('<DogList /> component', () => {
     wrapper.find('input').simulate('change',{target:{value:'Mix'}});
     expect(wrapper.find(DogCard).length).toEqual(2);
   });
+
+  it('should search by the entered term', () => {
+    const searchSpy = sinon.spy(DogList.prototype, 'searchDogs'); //spy for search method
+    const wrapper = shallow(<DogList />);
+    wrapper.find('input').simulate('change',{target:{value:'Mix'}});
+    expect(searchSpy.getCall(0).args[0]).toEqual('Mix');
+  })
 })
 
 describe('<AdoptPage> component', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<AdoptPage params={{dogName:'Fido'}} />);
+  });
+
   it('should adopt a dog on button click', () => {
-    const wrapper = shallow(<AdoptPage params={{dogName:'Fido'}} />);
     wrapper.find('button').simulate('click');
     expect(DOG_DATA[0].adopted).toEqual(true);
   });
 
   it('should show dog adopted on button cick', () => {
-    const wrapper = shallow(<AdoptPage params={{dogName:'Fido'}} />);
     wrapper.find('button').simulate('click');
     expect(wrapper.find('button').text()).toEqual('Already adopted!');
     expect(wrapper.find('button').props().disabled).toEqual(true);
